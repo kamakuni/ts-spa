3c49d288f789b8b45a43b367ca79c769b1d923bd
sugawarakensuke <superradio86@gmail.com>
2023-03-29 23:38:11 +0900

    Add card,cards method to find cards

--- api/app/src/cards/cards.service.ts
+++ api/app/src/cards/cards.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Card, Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { CreateCardDto } from './dto/create-card.dto';
 import { UpdateCardDto } from './dto/update-card.dto';
@@ -7,6 +8,29 @@ import { UpdateCardDto } from './dto/update-card.dto';
 export class CardsService {
 
   constructor(private prisma: PrismaService) {}
+
+  async card(cardWhereUniqueInput: Prisma.CardWhereUniqueInput,): Promise<Card | null> {
+    return this.prisma.card.findUnique({
+      where: cardWhereUniqueInput,
+    });
+  }
+
+  async cards(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.UserWhereUniqueInput;
+    where?: Prisma.UserWhereInput;
+    orderBy?: Prisma.UserOrderByWithRelationInput;
+  }): Promise<Card[]> {
+    const {skip, take, cursor, where, orderBy} = params;
+    return this.prisma.card.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
 
   create(createCardDto: CreateCardDto) {
     return 'This action adds a new card';
