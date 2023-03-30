34ecbf2ad7111ae17ad8cb8b5c698616adf45928
sugawarakensuke <superradio86@gmail.com>
2023-03-31 00:36:16 +0900

    Add list,lists method to find lists

--- api/app/src/lists/lists.service.ts
+++ api/app/src/lists/lists.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { List, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
 
@@ -9,6 +11,29 @@ export class ListsService {
 
   create(createListDto: CreateListDto) {
     return 'This action adds a new list';
+  }
+
+  async list(listWhereUniqueInput: Prisma.ListWhereUniqueInput,): Promise<List | null> {
+    return this.prisma.list.findUnique({
+      where: listWhereUniqueInput,
+    });
+  }
+
+  async lists(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.ListWhereUniqueInput;
+    where?: Prisma.ListWhereInput;
+    orderBy?: Prisma.ListOrderByWithRelationInput;
+  }): Promise<List[]> {
+    const {skip, take, cursor, where, orderBy} = params;
+    return this.prisma.list.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
   }
 
   findAll() {
